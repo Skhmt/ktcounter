92555649dc009a084caf367823c9e60a88890eda
Skhmt <Skhmt@users.noreply.github.com>
2023-12-07 13:36:49 -0800

    auto-subscribe

--- src/lib/TP.svelte
+++ src/lib/TP.svelte
@@ -1,22 +1,12 @@
 <script>
 	export let store;
-
-	let selected;
-
-	let unsubscribe = store.subscribe(value => {
-		selected = value;
-	});
-
-	function update() {
-		store.set(selected);
-	}
 </script>
 
 <label class="form-control flex flex-row mt-3">
 	<div class="label mr-1">
 		<span class="label-text-alt"><slot></slot></span>
 	</div>
-	<select class="select select-bordered select-sm" bind:value={selected} on:change={update}>
+	<select class="select select-bordered select-sm" bind:value={$store}>
 		<option selected>0</option>
 		<option>1</option>
 		<option>2</option>
