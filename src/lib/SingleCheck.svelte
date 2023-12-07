e15b3782560080a934e81cb105ab2a449c728b26
Skhmt <Skhmt@users.noreply.github.com>
2023-12-07 13:32:38 -0800

    simplified

--- src/lib/SingleCheck.svelte
+++ src/lib/SingleCheck.svelte
@@ -1,22 +1,10 @@
 <script>
 	export let store;
-
-	let check;
-
-	let unsubscribe = store.subscribe(value => {
-		check = value;
-	});
-
-	function update() {
-		store.set(check);
-	}
-	
-
 </script>
 
 <div class="form-control">
 	<label class="label cursor-pointer">
 		<span class="label-text"><slot></slot></span> 
-		<input type="checkbox" bind:checked={check} on:change={update} class="checkbox ml-3 bg-base-100" />
+		<input type="checkbox" bind:checked={$store} class="checkbox ml-3 bg-base-100" />
 	</label>
 </div>
