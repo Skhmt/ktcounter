6a34eec5257fad7926a1e411c3b5e1eaa7258b71
Skhmt <Skhmt@users.noreply.github.com>
2023-12-07 13:20:54 -0800

    Update Counter.svelte

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -3,23 +3,16 @@
 	import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons/index.js';
 
 	export let store;
-	export let preText;
-	export let postText;
 
 	const increment = () => {
 		store.update(x => x + 1);
 		window?.navigator?.vibrate?.(100);
 	};
-    const decrement = () => {
+
+    	const decrement = () => {
 		store.update(x => x > 0 ? x - 1 : 0);
 		window?.navigator?.vibrate?.(100);
-    };
-
-	let count;
-
-	const unsubscribe = store.subscribe(value => {
-		count = value;
-	});
+	};
 </script>
 
 <div class="flex flex-row">
@@ -27,14 +20,14 @@
 		<Fa icon={faMinus} />
 	</button>
 	<div class="flex flex-row px-4 pt-1 bg-base-100 rounded-btn border-primary text-neutral-content">
-		{#if preText}
-			<span class="text-sm pr-1">{preText}</span>
+		{#if $$slots.preText}
+			<span class="text-sm pr-1"><slot name="preText"></slot></span>
 		{/if}
 		<span class="countdown font-mono text-sm pt-1">
-			<span style="--value:{count}"></span>
+			<span style="--value:{$store}"></span>
 		</span>
-		{#if postText}
-			<span class="text-sm pl-1">{postText}</span>
+		{#if $$slots.postText}
+			<span class="text-sm pl-1"><slot name="postText"></slot></span>
 		{/if}
 	</div>
 	<button class="btn btn-accent btn-sm" on:click={increment} title="Increment">
