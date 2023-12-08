b6d84a0f22720745c6aac11b036545a51d5b38d5
skhmt <koshinn@gmail.com>
2023-12-07 17:49:17 -0800

    reset confirm

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ Has a PWA.
 ## Todo
 
 - [ ] Make initiative/attacker xor with the other player's initiative
-- [ ] Add modal/alert when resetting scores
 
+- [x] Add modal/alert when resetting scores
 - [x] Change reset to trash can icon
 - [x] TP1 has a bug with resetting
 - [x] Save to localStorage
--- src/lib/Reset.svelte
+++ src/lib/Reset.svelte
@@ -1,8 +1,12 @@
 <script>
+// @ts-nocheck
+
 	import Fa from 'svelte-fa';
 	import { faTrashCan } from '@fortawesome/free-solid-svg-icons/index.js';
 
 	import { store } from './stores.js';
+
+	let reset = document.getElementById('reset_modal');
 
 	function resetCounter() {
 		store.reset();
@@ -10,6 +14,25 @@
 	}
 </script>
 
-<button class="btn btn-secondary pt-4" on:click={resetCounter}  title="Reset Counter">
+<!-- <button class="btn btn-secondary pt-4" on:click={reset.showModal}  title="Reset Counter">
+	<Fa icon={faTrashCan} />
+</button> -->
+
+<!-- Open the modal using ID.showModal() method -->
+<button class="btn btn-secondary pt-4" title="Reset Counter" onclick="my_modal_5.showModal()">
 	<Fa icon={faTrashCan} />
 </button>
+<dialog id="my_modal_5" class="modal modal-bottom sm:modal-middle">
+	<div class="modal-box">
+		<h3 class="font-bold text-lg">Clear all scores?</h3>
+		<p class="py-4">Selecting "Reset" will wipe out all the scores saved.</p>
+		<p>This cannot be undone.</p>
+		<div class="modal-action">
+			<form method="dialog">
+				<!-- if there is a button in form, it will close the modal -->
+				<button class="btn mr-3" on:click={resetCounter}>Reset</button>
+				<button class="btn btn-accent">Cancel</button>
+			</form>
+		</div>
+	</div>
+</dialog>
