d85822af26e1d04f3346ed6566a30c03f1de5f7d
skhmt <koshinn@gmail.com>
2024-10-03 21:53:20 -0700

    Removed faction distinctions

--- src/lib/Faction.svelte
+++ src/lib/Faction.svelte
@@ -4,13 +4,9 @@
 	import teams from "../data/teams.js";
 
 	const bespokeFactions = [];
-	const compendiumFactions = [];
-	const narrativeFactions = [];
 
 	teams.forEach((v, k) => {
-		if (v.type == "bespoke") bespokeFactions.push(k);
-		else if (v.type == "compendium") compendiumFactions.push(k);
-		else narrativeFactions.push(k);
+		bespokeFactions.push(k);
 	});
 
 	let selected;
@@ -31,19 +27,9 @@
 	on:change={update}
 >
 	<option selected>Select Faction</option>
-	<optgroup label="Bespoke Teams">
+	<optgroup label=" ">
 		{#each bespokeFactions as bf}
 			<option value={bf}>{bf}</option>
-		{/each}
-	</optgroup>
-	<optgroup label="Compendium Teams">
-		{#each compendiumFactions as cf}
-			<option value={cf}>{cf}</option>
-		{/each}
-	</optgroup>
-	<optgroup label="Narrative Teams">
-		{#each narrativeFactions as nf}
-			<option value={nf}>{nf}</option>
 		{/each}
 	</optgroup>
 </select>
